979869cb2d6e1c02394e0c06511f9cb38fdc64fd
Víctor Bolaños Solís <viktorb.132@gmail.com>
2022-04-28 22:10:24 -0500

    First model letters changes

--- main.py
+++ main.py
@@ -44,7 +44,6 @@ def print_menu(menu_options):
 
 def optionOne():
     printTitle()
-    print('Ingresa una letra')
     randomWord = []
     with open("data.txt", "r", encoding="utf-8") as f:
         for line in f:
@@ -52,6 +51,7 @@ def optionOne():
     correctWord = randomWord[random.randint(0, len(randomWord)-1)]
     correctWord = correctWord[:-1]
     displayWord = codeWord(correctWord)
+    # print(displayWord)
     guessTheWord(displayWord, correctWord)
     play_loop()
 
@@ -66,12 +66,32 @@ def printTitle():
     print('╚═══════╩═╩═══════╩═╩═══════╝')
 
 def guessTheWord(word, aws):
-    print(' '.join(word))
-    # print(word, aws)
+    # aws = [i for i in aws]
+    start = 0
+    end = len(aws)
+    while(word != aws):
+        tempWord = [i for i in word]
+        print('Palabra: ', ' '.join(tempWord))
+        print(word, aws)
+        print('Ingresa una letra')
+        temCharacter = input()
+        # while(temCharacter in aws):
+        if temCharacter in aws:
+            print(temCharacter in aws)
+            print(aws.find(temCharacter, start, end))
+            changePosition = aws.find(temCharacter, start, end)
+            start = changePosition + 1
+            tempListWord = list(word)
+            tempListWord[changePosition] = temCharacter
+            word = ''.join(tempListWord)
+            # word[aws.index(temCharacter)] = temCharacter
+            print('Palabra: ', ' '.join(word))
+
+            # word = [temCharacter for i in aws if aws[i] == temCharacter]
 
 def codeWord(word):
     temp = ['_' for i in word]
-    return temp
+    return ''.join(temp)
 
 if __name__ == "__main__":
     main()
